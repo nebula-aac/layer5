e0a473b11780a845e016aa08c729977584f1d953
Kavitha Karunakaran <kavithakarunakaran1988@gmail.com>
2026-02-12 19:03:06 +0530

    Cleanup meetLinksData.js to be in sync with community calendar
    Signed-off-by: Kavitha Karunakaran <kavithakarunakaran1988@gmail.com>

--- src/sections/Community/Calendar/meetLinksData.js
+++ src/sections/Community/Calendar/meetLinksData.js
@@ -9,15 +9,6 @@ export const meet_links_data = [
     meeting_recordings: "https://www.youtube.com/playlist?list=PL3A-A6hPO2IO1ZuLj3cbOTdi-hlkF4Lqu",
   },
   {
-    day: "Tuesdays",
-    meeting: "Kanvas Meeting",
-    slack_channel: "#meshery-extensions",
-    slack_link: "https://mesheryio.slack.com/archives/C02F90W866T",
-    meeting_minutes: "https://bit.ly/3aXOaAs",
-    meeting_link: "https://meet.layer5.io/kanvas",
-    meeting_recordings: "https://www.youtube.com/playlist?list=PL3A-A6hPO2IO_yzN83wSJJUNQActzCJvO",
-  },
-  {
     day: "Wednesdays",
     meeting: "Meshery Development Meeting",
     slack_channel: "#meshery",
@@ -27,15 +18,6 @@ export const meet_links_data = [
     meeting_recordings: "https://www.youtube.com/playlist?list=PL3A-A6hPO2INMUzQc-9vT6Jz_VQQpsSS0",
   },
   {
-    day: "2nd & 4th Thursday",
-    meeting: "Meshery CI Meeting",
-    slack_channel: "#meshery-ci",
-    slack_link: "https://layer5io.slack.com/archives/C011VPREG2Z",
-    meeting_minutes: "https://docs.google.com/document/d/1GrVdGHZAYeu6wHNLLoiaKNqBtk7enXE9XeDRCvdA4bY/edit",
-    meeting_link: "https://meet.meshery.io/ci",
-    meeting_recordings: "https://www.youtube.com/playlist?list=PL3A-A6hPO2IM7rYiKxG4l3eQNc6X3IUex",
-  },
-  {
     day: "Fridays",
     meeting: "Layer5 Newcomers' Meeting",
     slack_channel: "#newcomers",
@@ -43,15 +25,6 @@ export const meet_links_data = [
     meeting_minutes: "https://docs.google.com/document/d/1aHarIyIL__HRq_4klYOUsEXP5SQLlmb50W17yKdNmpU/edit#",
     meeting_link: "https://meet.layer5.io/newcomers",
     meeting_recordings: "https://www.youtube.com/playlist?list=PL3A-A6hPO2INVEknQ4R-VwDMUT9yM_FSm",
-  },
-  {
-    day: "Fridays",
-    meeting: "Layer5 Community Meeting",
-    slack_channel: "#general",
-    slack_link: "https://layer5io.slack.com/archives/CDM0ACDM5",
-    meeting_minutes: "https://bit.ly/2It7IA4",
-    meeting_link: "https://meet.layer5.io/community",
-    meeting_recordings: "https://www.youtube.com/playlist?list=PL3A-A6hPO2IMPPqVjuzgqNU5xwnFFn3n0",
   },
   {
     day: "Fridays",
